3c89795a048929deed329a9064557c252410c1ff
AlexandreLeriche141282 <139038579+AlexandreLeriche141282@users.noreply.github.com>
2025-06-26 20:33:27 +0200

    nav+header

---
+++ index.js
@@ -0,0 +1,14 @@
+// ----------- Menu Burger ---------------------- //
+
+const toggleMenu = (action) => {
+  document.querySelector('nav ul').classList[action]('active');
+  document.querySelector('.menu-burger').classList[action]('active');
+  // document.querySelector('.menu-background').classList[action]('active');
+};
+
+document.querySelector('.menu-burger').addEventListener('click', () => toggleMenu('toggle'));
+document.querySelector('.closeMenu').addEventListener('click', () => toggleMenu('remove'));
+// document.querySelector('.menu-background').addEventListener('click', () => toggleMenu('remove'));
+document.querySelectorAll('.link1, .link2, .link3, .link4, .link5, .link6, .link7').forEach(link => {
+    link.addEventListener('click', () => toggleMenu('remove'));
+});
