d9522ead806baef694b1658a435435db99eace64
AlexandreLeriche141282 <139038579+AlexandreLeriche141282@users.noreply.github.com>
2025-06-28 10:04:19 +0200

    nav style 4

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ document.querySelector('.closeMenu').addEventListener('click', () => toggleMenu(
 document.querySelectorAll('.link1, .link2, .link3, .link4, .link5, .link6, .link7').forEach(link => {
     link.addEventListener('click', () => toggleMenu('remove'));
 });
+
+
+// ----------- Apparition et disparition nav --------- //
+
+const nav = document.querySelector("nav");
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < lastScroll) {
+        nav.style.top ="0px";
+    } else {
+        nav.style.top = "-140px"
+    }
+    lastScroll = window.scrollY;
+
+});
+// ---------------------------------------------------------
